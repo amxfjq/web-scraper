c42ad557edc22159b265b9ccc80bae6d9489dd76
amxfjq <amxfjq@Amxfjq>
2023-09-18 18:28:34 +0100

    I'm done with the wiki page, moving to how to manipulate multi pages

--- richest_companies_by_revenue/richest_companies_by_revenue/spiders/first_spider.py
+++ richest_companies_by_revenue/richest_companies_by_revenue/spiders/first_spider.py
@@ -7,4 +7,18 @@ class FirstSpiderSpider(scrapy.Spider):
     start_urls = ["https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"]
 
     def parse(self, response):
-        pass
+        xxx = response.css('tbody tr')[2:-15]
+        list_of_companies_and_info = xxx.css('tr')
+        for i in list_of_companies_and_info:
+            yield{
+                'rank': i.css('th::text').get().strip(),
+                'company': i.css('td a::text')[0].get(),
+                'industry': i.css('td a::text')[1].get(),
+                'revenue(in millions)': i.css('td')[2].css('td::text').get().strip(),
+                'profit(in millions)': i.css('td')[3].css('td::text').get().strip(),
+                'employees': i.css('td')[4].css('td::text').get().strip(),
+                'headquarters': i.css('td')[5].css('td::text').get().strip(),
+                'state owned': i.css('td')[6].attrib['data-sort-value']
+
+            }
+            
