dc105fc1a399f398c72acdd188d9f928509dd35d
amxfjq <amxfjq@Amxfjq>
2023-09-19 18:02:13 +0100

    a little bit into items and pipelines

--- richest_companies_by_revenue/richest_companies_by_revenue/items.py
+++ richest_companies_by_revenue/richest_companies_by_revenue/items.py
@@ -10,3 +10,16 @@ class RichestCompaniesByRevenueItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class CompanyInfo(scrapy.Item):
+    rank = scrapy.Field()
+    company = scrapy.Field()
+    industry = scrapy.Field()
+    revenue_in_millions = scrapy.Field()
+    profit_in_millions = scrapy.Field()
+    employees = scrapy.Field()
+    headquarters = scrapy.Field()
+    state_owned = scrapy.Field()
+    company_name = scrapy.Field()
+    company_type = scrapy.Field()
+    website = scrapy.Field()
--- richest_companies_by_revenue/richest_companies_by_revenue/spiders/first_spider.py
+++ richest_companies_by_revenue/richest_companies_by_revenue/spiders/first_spider.py
@@ -1,36 +1,38 @@
 import scrapy
-
-
+from richest_companies_by_revenue.items import CompanyInfo
 class FirstSpiderSpider(scrapy.Spider):
     name = "first_spider"
     allowed_domains = ["en.wikipedia.org"]
     start_urls = ["https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"]
 
     def parse(self, response):
-        long_shot = []
+        details = CompanyInfo()
+        #long_shot = []
         xxx = response.css('tbody tr')[2:-15]
         list_of_companies_and_info = xxx.css('tr')
         for i in list_of_companies_and_info:
-            yield{
-                'rank': i.css('th::text').get().strip(),
-                'company': i.css('td a::text')[0].get(),
-                'company info':i.css('a::attr(href)').get(),
-                'industry': i.css('td a::text')[1].get(),
-                'revenue(in millions)': i.css('td')[2].css('td::text').get().strip(),
-                'profit(in millions)': i.css('td')[3].css('td::text').get().strip(),
-                'employees': i.css('td')[4].css('td::text').get().strip(),
-                'headquarters': i.css('td')[5].css('td::text').get().strip(),
-                'state owned': i.css('td')[6].attrib['data-sort-value']
+            details['rank'] = i.css('th::text').get().strip(),
+            details['company'] = i.css('td a::text')[0].get(),
+            details['company info'] = i.css('a::attr(href)').get(),
+            details['industry'] = i.css('td a::text')[1].get(),
+            details['revenue_in_millions'] = i.css('td')[2].css('td::text').get().strip(),
+            details['profit_in_millions'] = i.css('td')[3].css('td::text').get().strip(),
+            details['employees'] = i.css('td')[4].css('td::text').get().strip(),
+            details['headquarters'] = i.css('td')[5].css('td::text').get().strip(),
+            details['state owned'] = i.css('td')[6].attrib['data-sort-value']
 
-            }
-            long_shot.append(i.css('a::attr(href)').get())
+            #long_shot.append(i.css('a::attr(href)').get())
 
-        for i in long_shot:
+            yield CompanyInfo
+
+'''        for i in long_shot:
             actual_url = 'https://en.wikipedia.org/' + i
             yield response.follow(actual_url, callback=self.parse_company_info)
 
     def parse_company_info(self, response):
-        company_type = response.css('table.infobox.vcard tbody tr td.infobox-data.category')
+        i = response.css('table.infobox.vcard tbody tr td')
         yield{
-            'type': company_type.css('a::text').get()
-        }
+            'company_name': response.css('table.infobox.vcard caption::text').get(),
+            'company_type': i.css('td.infobox-data.category a::text').get(),
+            'website': i.css('span.url a::attr(href)').get()
+        }'''
