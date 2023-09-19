d66766a8b16715453a1883df396697d9472dda87
amxfjq <amxfjq@Amxfjq>
2023-09-19 06:20:19 +0100

    i scraped the data in a linked page on a website, used wiki

--- richest_companies_by_revenue/richest_companies_by_revenue/spiders/first_spider.py
+++ richest_companies_by_revenue/richest_companies_by_revenue/spiders/first_spider.py
@@ -7,12 +7,14 @@ class FirstSpiderSpider(scrapy.Spider):
     start_urls = ["https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"]
 
     def parse(self, response):
+        long_shot = []
         xxx = response.css('tbody tr')[2:-15]
         list_of_companies_and_info = xxx.css('tr')
         for i in list_of_companies_and_info:
             yield{
                 'rank': i.css('th::text').get().strip(),
                 'company': i.css('td a::text')[0].get(),
+                'company info':i.css('a::attr(href)').get(),
                 'industry': i.css('td a::text')[1].get(),
                 'revenue(in millions)': i.css('td')[2].css('td::text').get().strip(),
                 'profit(in millions)': i.css('td')[3].css('td::text').get().strip(),
@@ -21,4 +23,14 @@ class FirstSpiderSpider(scrapy.Spider):
                 'state owned': i.css('td')[6].attrib['data-sort-value']
 
             }
-            
+            long_shot.append(i.css('a::attr(href)').get())
+
+        for i in long_shot:
+            actual_url = 'https://en.wikipedia.org/' + i
+            yield response.follow(actual_url, callback=self.parse_company_info)
+
+    def parse_company_info(self, response):
+        company_type = response.css('table.infobox.vcard tbody tr td.infobox-data.category')
+        yield{
+            'type': company_type.css('a::text').get()
+        }
